8174ff7c56064b37e5bc4d0ba56a2c214ebe46a2
vwallajabad <vwallajabad@gmail.com>
2024-02-18 12:20:37 -0600

    made my code more pretty and added a typewriter effect and empty card div

---
+++ src/script/typewriter.js
@@ -0,0 +1,6 @@
+const instance = new Typewriter(document.getElementById("typewriter"), {
+  strings: ["grow", "evolve", "thrive", "learn", "lead", "explore"],
+  autoStart: true,
+  delay: 75,
+  loop: true,
+});
