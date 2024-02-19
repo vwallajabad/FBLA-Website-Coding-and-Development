b309ab3e078b47d91b957ebff57d644a33c28645
vwallajabad <vwallajabad@gmail.com>
2024-02-19 09:14:16 -0600

    added a templete card for benefits.html and optomized SEO for jobs.html

--- src/script/typewriter.js
+++ src/script/typewriter.js
@@ -1,5 +1,5 @@
 const instance = new Typewriter(document.getElementById("typewriter"), {
-  strings: ["grow", "evolve", "thrive", "learn", "lead", "explore"],
+  strings: ["grow", "evolve", "thrive", "learn", "lead", "explore", "innovate"],
   autoStart: true,
   delay: 75,
   loop: true,
