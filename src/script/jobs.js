fc42692b677288f25267a345b57d3b67ce970342
vwallajabad <vwallajabad@gmail.com>
2024-03-16 14:24:31 -0500

    asdf

--- src/script/jobs.js
+++ src/script/jobs.js
@@ -2,3 +2,12 @@ var input = document.getElementById('search_bar_id');
 
 input.setAttribute('size',input.getAttribute('placeholder').length);
 
+$(window).on('resize', function() {
+    if ($(window).width() < 500) {
+       $('.col').removeClass('col');
+    } else {
+       // If you want to add the class back when the window is resized to be larger than 500px
+       // $('.col').addClass('col');
+    }
+   }).trigger('resize'); // Trigger the resize event on page load
+   
