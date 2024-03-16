546ac3e5f2d9b4d56d0f449d9d1d18911c29fba7
vwallajabad <vwallajabad@gmail.com>
2024-03-16 14:28:15 -0500

    debug

--- src/script/jobs.js
+++ src/script/jobs.js
@@ -3,7 +3,8 @@ var input = document.getElementById('search_bar_id');
 input.setAttribute('size',input.getAttribute('placeholder').length);
 
 $(window).on('resize', function() {
-    if ($(window).width() < 500) {
+    if ($(window).width() < 700) {
+        alert('window is smaller than 500px');
        $('.col').removeClass('col');
     } else {
        // If you want to add the class back when the window is resized to be larger than 500px
