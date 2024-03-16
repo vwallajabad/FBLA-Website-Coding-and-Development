f56b0a94550488450182c4588d11535f7d35edc0
vwallajabad <vwallajabad@gmail.com>
2024-03-16 15:07:13 -0500

    fixed the x-scroll bug

--- src/script/jobs.js
+++ src/script/jobs.js
@@ -3,11 +3,9 @@ var input = document.getElementById('search_bar_id');
 input.setAttribute('size',input.getAttribute('placeholder').length);
 
 $(window).on('resize', function() {
-    if ($(window).width() < 700) {
-        alert('window is smaller than 500px');
+    if ($(window).width() < 600) {
        $('.col').removeClass('col');
     } else {
     }
    }).trigger('resize');
    
-   console.log($(window).width());
