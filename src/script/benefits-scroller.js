e6b7302e086516890c047d82ed444f5c3b3ff67f
ichaudhari <ishac1017@gmail.com>
2024-04-08 17:32:45 -0500

    new-benefits

--- src/script/benefits-scroller.js
+++
@@ -1,7 +0,0 @@
-var splide = new Splide( '.splide', {
-    direction: 'ttb',
-    height   : '10rem',
-  } );
-  
-  splide.mount();
-  
--- src/script/slide1.js
+++
@@ -1,5 +0,0 @@
-gsap.to(".slide1", {
-    x: 700,
-    rotation: 360,
-    duration: 3
-})
